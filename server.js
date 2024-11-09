052d7dfe871eba9160365225a4e118529e65030e
Makoman <elgamlahmed@gmail.com>
2024-11-09 20:11:01 +0200

    final edits to code

--- server.js
+++ server.js
@@ -1,40 +1,51 @@
 // Setup empty JS object to act as endpoint (consider using an array/object structure)
 let projectData = {};
+let baseURL = "https://api.openweathermap.org/data/2.5/weather?q=";
+let apiKey = "1b14c9a59e2c27e33ed684a98d4a0513";
 
 // Require Express to run server and routes
-const express = require('express');
+const express = require("express");
 
 // Start up an instance of app
 const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-const cors = require('cors');
+const cors = require("cors");
+const { default: test } = require("node:test");
 app.use(cors());
 
 // Initialize the main project folder
-app.use(express.static('website'));
+app.use(express.static("website"));
 
 // Setup Server
-const port = 3000;
+const port = 4000;
 const server = app.listen(port, () =>
   console.log(`running on localhost: ${port}`)
 );
+const getWeather = async (newWeather) => {
+  const res = await fetch(baseURL + newWeather + "&appid=" + apiKey);
 
-// GET route server side - Returns projectData (modify to return specific data)
-app.get('/', (req, res) => {
-  res.json(projectData);
+  try {
+    const data = await res.json();
+    projectData.temp = data.main.temp;
+    console.log(projectData);
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+app.post("/", async (req, res) => {
+  const { newWeather } = req.body;
+  await getWeather(newWeather);
+  res.json({});
 });
 
-// POST route server side - Receives weather data and updates projectData
-app.post('/', (req, res) => {
-    const { feelings } = req.body;
-    projectData.feelings = feelings;
-  
-    // You might want to store this data in a database or log it here
-    console.log(projectData);})
+app.get("/weather", (req, res) => {
+  res.json(projectData);
+});
--- website/app.js
+++ website/app.js
@@ -1,47 +1,34 @@
 /* Global Variables */
 // Personal API Key for OpenWeatherMap API
-
-let baseURL = 'https://api.openweathermap.org/data/2.5/weather?q='
-let apiKey = '1b14c9a59e2c27e33ed684a98d4a0513';
-let newWeather = document.getElementById('zip').value;
-
-document.getElementById('generate').addEventListener('click', performAction);
-
-async function performAction(){  
-
-// Fetching weather info
-  const getWeather = (baseURL, apiKey) => {
-  getWeather(baseURL,document.getElementById('zip').value, apiKey)}
-  const res = await fetch(baseURL+document.getElementById('zip').value+'&appid='+apiKey)
-  
-  try {
-
-    const data = await res.json();
-    console.log(data)
-    document.getElementById('temp').innerHTML = Math.round(data.temp)+ 'degrees';
-    document.getElementById('content').innerHTML = data.feel;
-    document.getElementById('date').innerHTML =data.date;
-    }
-    catch(error) {
-      console.log("error", error);
-      // appropriately handle the error
-    }
-}
-
-let input = document.getElementById('feelings').value
-
-fetch(baseURL+document.getElementById('zip').value+'&appid='+apiKey, {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(input)
-})
-.then(response => response.json())
-.then(input => console.log(input))
-.catch(error => console.error('Error:',   
- error));
-
+let newWeather = document.getElementById("zip");
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
+
+document.getElementById("generate").addEventListener("click", performAction);
+
+async function performAction() {
+  // Fetching weather info
+  fetch("http://localhost:4000/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ newWeather: newWeather.value }),
+  }).then(async () => {
+    const res = await fetch("http://localhost:4000/weather", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const { temp } = await res.json();
+    console.log(temp);
+    document.getElementById("temp").innerHTML = Math.round(temp) + " degrees";
+    // fix the label area
+    document.getElementById("content").innerHTML =
+      document.getElementById("feelings").value;
+    // fix the date
+    document.getElementById("date").innerHTML = newDate;
+  });
+}
